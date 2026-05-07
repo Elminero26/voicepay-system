b2629d45eab383fcdab73415f178f5448883d154
Tu Nombre <tu-email@ejemplo.com>
2026-05-07 18:43:17 +0200

    feat: initialize notification service application with OpenAPI documentation and API key security configuration

--- notification-service/src/main/java/com/voicepay/notification/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/voicepay/notification/NotificationServiceApplication.java
@@ -1,9 +1,25 @@
 package com.voicepay.notification;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(title = "Notification Service API", version = "1.0", description = "Servicio para gestionar y enviar notificaciones"),
+		security = @SecurityRequirement(name = "X-API-KEY")
+)
+@SecurityScheme(
+		name = "X-API-KEY",
+		type = SecuritySchemeType.APIKEY,
+		in = SecuritySchemeIn.HEADER,
+		paramName = "X-API-KEY"
+)
 public class NotificationServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
